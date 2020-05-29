5046842ae0bba9ada17e129a0dbc6a72f59fcd02
LucasnDomingues <lucasn.domingues@gmail.com>
2020-05-29 18:03:04 -0300

    Initial commit

---
+++ src/LinkedList/Node.js
@@ -0,0 +1,7 @@
+export default class Node{
+    
+    sayHello(){
+        console.log('Hello World!');
+    }
+    
+}
---
+++ src/Tester.js
@@ -0,0 +1,5 @@
+import Node from './LinkedList/Node';
+
+console.log("Starting...");
+const node = new Node();
+node.sayHello();
